2b832e1d500230e7ebeaa464c30084f9d1586860
Imran Raja <imranraja85@gmail.com>
2016-10-09 07:10:00 -0400

    set login path

--- app/controllers/admin/admins_controller.rb
+++ app/controllers/admin/admins_controller.rb
@@ -1,4 +1,8 @@
 class Admin::AdminsController < ApplicationController
   protect_from_forgery with: :exception
   before_action :authenticate_admin!
+
+  def after_sign_in_path_for(resource)
+    admin_hackers_path
+  end
 end
