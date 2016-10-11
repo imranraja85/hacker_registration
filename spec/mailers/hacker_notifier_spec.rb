988278448c8200889e07c883de86872bf14fa409
Imran Raja <imranraja85@gmail.com>
2016-10-11 19:15:21 -0400

    mailer spec

--- spec/mailers/hacker_notifier_spec.rb
+++ spec/mailers/hacker_notifier_spec.rb
@@ -1,5 +1,15 @@
 require "rails_helper"
 
 RSpec.describe HackerNotifierMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'tests' do
+    hacker = create(:hacker)
+    email = HackerNotifierMailer.send_winner_email(hacker)
+    email.deliver_now
+
+    expect(email.from).to eq(['noreply@example.com'])
+    expect(email.to).to eq(['ace@example.com'])
+    expect(email.subject).to eq("Congratulations! You've the won raffle. Come claim your prize!")
+binding.pry
+    expect(email.body.to_s).to eq('hello')
+  end
 end
