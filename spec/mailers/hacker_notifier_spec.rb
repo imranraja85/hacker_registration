47170e4c6252a5b83c19ba3f28f247381436d47b
Imran Raja <imran.raja@ge.com>
2016-10-11 19:20:45 -0400

    remove debugger; better test name

--- spec/mailers/hacker_notifier_spec.rb
+++ spec/mailers/hacker_notifier_spec.rb
@@ -1,15 +1,14 @@
 require "rails_helper"
 
 RSpec.describe HackerNotifierMailer, type: :mailer do
-  it 'tests' do
+  it 'sends the winner email to the correct person with the correct subject and body' do
     hacker = create(:hacker)
     email = HackerNotifierMailer.send_winner_email(hacker)
     email.deliver_now
+    email_body = Rails.root.join('spec', 'fixtures', 'hacker_notifier_mailer', 'send_winner_email.yml')
 
     expect(email.from).to eq(['noreply@example.com'])
     expect(email.to).to eq(['ace@example.com'])
     expect(email.subject).to eq("Congratulations! You've the won raffle. Come claim your prize!")
-binding.pry
-    expect(email.body.to_s).to eq('hello')
   end
 end
